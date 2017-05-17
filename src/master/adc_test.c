0e55dae9ed617c909208d35fcea829c9abef730a
Arne Dussin <arne.dussin@live.de>
2017-05-17 16:32:51 +0200

    Added control LED

--- src/master/adc_test.c
+++ src/master/adc_test.c
@@ -15,8 +15,8 @@ void main(void) {
 	WDTCTL = WDTPW + WDTHOLD;
 
 	// Set Up the test LED.
-	P1DIR |= (BIT6);
-	P1OUT &= ~(BIT6);
+	P1DIR |= (BIT4 + BIT6);
+	P1OUT &= ~(BIT4 + BIT6);
 
 	adc_init_inputs();
 
@@ -27,5 +27,15 @@ void main(void) {
 		// Write the new values to the output.
 		char values[3] = {delta_Z, delta_H, delta_Y};
 		ssbl_write(values);
+
+		// Test-LED, to see, how much in sync master and slave are.
+		// (In terms of translation, the LED is on when the Zeppelin is going up)
+		if (delta_H > input_range / 2) {
+			P1OUT |= BIT4;
+		}
+		else {
+			P1OUT &= ~BIT4;
+		}
+
 	}
 }
